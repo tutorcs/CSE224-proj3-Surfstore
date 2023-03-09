e926d9524c662deaec72aa81a544961face008ea
tutorcs <tutorcs@163.com>
2023-03-09 09:46:08 +0800

    WeChat cstutorcs CSE224 proj3 Surfstore

---
+++ pkg/surfstore/SurfStoreConstants.go
@@ -0,0 +1,17 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+package surfstore
+
+const DEFAULT_META_FILENAME string = "index.db"
+
+const TOMBSTONE_HASHVALUE string = "0"
+const EMPTYFILE_HASHVALUE string = "-1"
+
+const FILENAME_INDEX int = 0
+const VERSION_INDEX int = 1
+const HASH_LIST_INDEX int = 2
+
+const CONFIG_DELIMITER string = ","
+const HASH_DELIMITER string = " "
---
+++ pkg/surfstore/SurfstoreUtils.go
@@ -0,0 +1,10 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+package surfstore
+
+// Implement the logic for a client syncing with the server here.
+func ClientSync(client RPCClient) {
+	panic("todo")
+}
